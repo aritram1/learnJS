687d7c14e7b6aff9a82b632b67269be2dad11357
aritram1 <aritram1@gmail.com>
2020-05-08 00:28:07 +0530

    Update benchmark.js

--- BenchmarkResults/benchmark.js
+++ BenchmarkResults/benchmark.js
@@ -6,9 +6,13 @@ river=Array.from('mississipi');
 char='m'
 river.reverse().indexOf(char);
 river.lastIndexOf(char); //59% slower
-// Let's change the character from 'm' to 's'
+// Arrangement of the char in the string matters. Let's change the character from 'm' to 's'
 char='s'
 river.reverse().indexOf(char);
-river.lastIndexOf(char); //23% slower
+river.lastIndexOf(char); //still 23% slower
+
+// Considering all characters
+river.forEach(char => { river.reverse().indexOf(char);});
+river.forEach(char => { river.lastIndexOf(char); }); // 19% slower
 
 // ----------------------------------------------------------------------------------------------
