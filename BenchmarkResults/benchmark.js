e56e54e6ca2096bfa510fb9cdfcd596e2cd77885
aritram1 <aritram1@gmail.com>
2020-05-08 00:24:07 +0530

    Update benchmark.js

--- BenchmarkResults/benchmark.js
+++ BenchmarkResults/benchmark.js
@@ -3,7 +3,12 @@
 // Let's see two ways of finding presence of a character in a string. indexOf(char) returns the first occurence 
 // where as lastIndexOf(char) returns the last occurence
 river=Array.from('mississipi');
+char='m'
+river.reverse().indexOf(char);
+river.lastIndexOf(char); //59% slower
+// Let's change the character from 'm' to 's'
 char='s'
 river.reverse().indexOf(char);
 river.lastIndexOf(char); //23% slower
+
 // ----------------------------------------------------------------------------------------------
