a745a999d6386af97933a769112b21f556830dd4
aritram1 <aritram1@gmail.com>
2020-05-09 03:24:17 +0530

    Update StrangeCodes.js

--- StrangeCodes.js
+++ StrangeCodes.js
@@ -1,15 +1,17 @@
 // 1 - All codes in wtf.js :D
 // -------------------------------------------------------------------------------------------
-// 2 - This function which does many things, still returns output same as input
-doNothing = function(char){
-  typeOf(this) != 'string' ? return : ;
-  that = p.slice.call(Array.from(this), 
-                      this.indexOf(char), 
-                      this.lastIndexOf(char)+1)
-                .join('');
-  return that;// === this;
-}
+// 2 - This function which does many things, still returns output same as input. It works same as its name ;-)
+// -------------------------------------------------------------------------------------------
+// 3 - Incomplete
+// func = function(){
+//   if (this[0] != this[this.length-1]) return;
+//   char = this[0];
+//   that = Array.prototype.slice.call(Array.from(this), // splits
+//                       this.indexOf(char), 
+//                       this.lastIndexOf(char)+1)
+//                 .join('');                            // joins
+//   return that;
+// }
 
-//Usage : YouName.doNothing();
-
-'aritra'.doNothing();
+// //Usage : func.call('aritra'); //Output 'aritra'
+// -------------------------------------------------------------------------------------------
