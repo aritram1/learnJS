9ed1dfe2a1807f566c25a5e926b7f9d0c798f541
aritram1 <aritram1@gmail.com>
2020-05-25 23:37:15 +0530

    Create filesystem.js

---
+++ Frameworks/Node/filesystem/filesystem.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var myutil = require('./myutil');
+
+// open file
+const tempfile = './data_temp';
+var newFile1 = fs.open(tempfile, 'w', function(err, data){
+    if(!err) console.log(`${tempfile} File opened`);
+    fs.close(data, function(err, data){
+        if(!err) console.log(`${tempfile} File closed`);
+    });
+});
+
+//read file
+const file = './data';
+var newFile2 = fs.readFile(file, 'utf8', function(err, data){
+    if(!err){
+        console.log(`${file} File opened`);
+        console.log('content : ' + data);
+    }
+});
+
+// append file
+const aritra = myutil.generate('aritra', 'mukherjee', 'Mr.', 'Javascript');
+const moniseeta = myutil.generate('moniseeta', 'roy', 'Mrs.', 'Docker');
+const bhai = myutil.generate('aritra', 'roy', 'Mr.', 'Flutter');
+entries = aritra + moniseeta + bhai;
+var newFile3 = fs.appendFile('./data', entries, function(err, data){
+    if(!err){
+        console.log('After appending data');
+        console.log(data);
+    }
+});
