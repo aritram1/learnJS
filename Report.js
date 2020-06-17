3c6a30b59fd5626ac7aceaf934ac2bdf884f9db8
aritram1 <aritram1@gmail.com>
2020-06-17 22:35:36 +0530

    Update Report.js

--- Report.js
+++ Report.js
@@ -15,10 +15,44 @@ fetch('https://api.covid19india.org/raw_data6.json')
     console.log('Error occurred to fetch data: ', error);
 })
 
-report_state={};
-for (r of _data){
-    if (r.detectedstate in report_state)
-        report_state[r.detectedstate] = report_state[r.detectedstate]+1;
-    else
-        report_state[r.detectedstate] = 0;
+//Random
+'length' in ['width', 'breadth', 'height'] //Outputs true. LOL
+
+keys = Object.keys(_data[0]);
+
+// A function that can group the data by the field provided for n number of data
+// Usage : groupBy('nationality', 100)
+// outputs group by nationality count for first 100 records
+groupBy = function(field, count){ //count is optional
+    if (!field) console.log('group by what?');return;
+    report = {};
+    data = count ? _data.slice(0, count) : _data; //limiting data
+    if (keys.includes(field)){
+        for(row of data){
+            value = row[field];
+            report[value] = value in report 
+                ? report[value] + 1 // If entry is present increment it
+                : 1;                // If entry is not present, enter it.
+        }
+    }
+    else console.log('Field not present');
+    return report;
+}
+
+//backup
+groupBy = function(field, count){ //count is optional
+    if (!field) console.log('group by what?');return;
+    report = {};
+    data = count ? _data.slice(0, count) : _data; //limiting data
+    if (keys.includes(field)){
+        for(row of data){
+            value = row[field];
+            report[value] = value in report 
+                ? report[value] + 1 // If entry is present increment it
+                : 1; debugger;               // If entry is not present, enter it.
+        }
+debugger;
+    }
+    else console.log('Field not present');
+    return report;
 }
