c7ab5b6c757ff7c6e88db17172c5c2405bdf72b0
aritram1 <aritram1@gmail.com>
2020-07-05 18:54:29 +0530

    Create PromiseUnderstanding4.js

---
+++ Promise/PromiseUnderstanding4.js
@@ -0,0 +1,43 @@
+var testnum = function(num){
+    return new Promise((res, rej) => {
+        result = {};
+        if(num > 200){
+            result.msg = 'greater than 200';
+            res(result);
+        }
+        else if(num > 100){
+            result.msg = 'greater than 100';
+            res(result);
+        }
+        else if(num > 75){
+            result.msg = 'can not be between 76-99';
+            rej(result);
+        }
+        else{
+            return new Promise((rs,rj)=>{
+                if(num>50){
+                    result.msg='greater than 50';
+                    rs(passIt(result));
+                }
+                else 
+                    result.msg='can not be between must be greater than 50';
+                    rj(passIt(result));
+            })
+            .then(_data=> console.log('Data is : ' + _data.msg))
+            .catch(_err => console.log('Err0r : ' + _err.msg));
+        }
+    })
+    .then(_data => {
+        console.log('Data is : ' + _data.msg);
+    })
+    .catch(_err =>{
+        console.log('Error : ' + _err.msg);
+    })
+}
+passIt = msg => msg;
+[300, 200, 100, 50].forEach(i=>testnum(i));
+
+
+// Data is : greater than 200
+// Data is : greater than 100
+// Error : can not be between 76-99
